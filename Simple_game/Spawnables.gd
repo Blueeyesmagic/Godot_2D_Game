467f5f44427b3b8d6641ee0175084b9b9e1186c0
Blueeyesmagic <90469493+Blueeyesmagic@users.noreply.github.com>
2024-09-15 16:36:40 +0200

    Add files via upload
    Addet crate textures, explosion particales, TileMap for designing maps

--- Simple_game/Spawnables.gd
+++ Simple_game/Spawnables.gd
@@ -7,10 +7,18 @@ var explosive: bool
 var exp_force: float
 var health: int
 var damage: int
-var texture: Texture2D
+var crate_name:String
+var object
+@onready var sprite = $Sprite2D
+@onready var explosion = $GPUParticles2D
+@onready var timer = $Timer
+@onready var area = $Area2D
 func _ready():
 	wich_crate()
-			#edit texture
+	print("crate_name")
+	sprite.texture = load("res://Textures/Crates/"+crate_name+".jpg")
+	timer.connect("timeout", Callable(self, "_on_destruction_timer_timeout"))
+	timer.one_shot = true		#edit texture
 	
 func take_damage(hit_damage:int):
 	health-=hit_damage
@@ -18,30 +26,53 @@ func take_damage(hit_damage:int):
 		queue_free()
 func give_damage():
 	return damage
-func explode(object: Node2D):
+#Array of objects im umkreis und das dann Loopen
+func explode():
 	if explosive:
 		print("explode")
-		var direction = object.position-position
-		object.linear_velocity +=direction*exp_force
-		#queue_free()
+		getbodies()
+		explosion.emitting = true
+		sprite.texture=null
+		explosive=false
+		timer.wait_time = 1  # or a fixed time if you know it
+		timer.start()
 func wich_crate():
 		match crate: 
 			0: #normal
 				normal_crate=true
 				health=100
+				if crate_name =="":
+					crate_name = "Normal_crate"
 				#edit texture
 			1: #harder
 				harder_crate=true
 				health=200
+				if crate_name =="":
+					crate_name = "hard_crate"
 				#edit texture
 
 			2: #explosive
 				explosive=true
 				health=1
 				exp_force= randi_range(10,15)
-				
 				damage = 50
+				if crate_name =="":
+					crate_name = "Explosive_crate"
 				#edit texture
 			3: #random state
 				crate=randi_range(0,2)
+				if crate_name =="":
+					crate_name = "random_crate"
 				wich_crate()
+				
+func _on_timer_timeout():
+	print("zuende")
+	queue_free()
+func getbodies():
+	for object in area.get_overlapping_bodies():
+		print(object)
+		if object is RigidBody2D:
+			print("ridgetbody")
+			var direction = object.position-position
+			object.linear_velocity= direction*exp_force
+		
